a74a0f196c22d9d6d8df9e6075e145fa31132828
ashwini0724 <ashwinimaheshwari.07@gmail.com>
2025-03-20 19:54:51 +0530

    Fix GitHub Pages deployment

--- VueCommerce/src/router/index.js
+++ VueCommerce/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import ProductPage from '@/components/ProductGrid.vue';
 
 
@@ -7,8 +7,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
+    history: createWebHashHistory(),
+    routes
+  });
 
 export default router;
